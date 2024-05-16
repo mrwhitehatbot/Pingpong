ecea042551d4a934f0696b157838093ddf742bc8
mrwhitehatbot <168001286+mrwhitehatbot@users.noreply.github.com>
2024-05-16 11:39:16 +0530

    Update pong_server.py

--- pong_server.py
+++ pong_server.py
@@ -6,7 +6,7 @@ import pygame, sys
 from pygame.locals import *
 import time
 import socket
-ip=input("Enter Ip Address:")
+ip=input("Enter Your Ip Address:")
 name=input("Enter Your Name:")
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = ip
